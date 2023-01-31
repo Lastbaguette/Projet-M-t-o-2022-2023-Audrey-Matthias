cee48c4878f73400f0277240d3df77ed48a2b9a6
Crystale14 <crystale.trg@outlook.com>
2023-01-31 13:59:20 +0100

    more comments

--- meteo/weathersort.h
+++ meteo/weathersort.h
@@ -17,62 +17,63 @@ typedef struct _date {
 }Date;
 
 typedef struct _station1 {
-    int ID;
+    int ID;     //Station ID
 
-    float min;
-    float max;
+    float min;      //Will contain min value reached for a certain date or ID
+    float max;      //Will contain max value reached for a certain date or ID
 
-    int count;
+    int count;      //All sorts : will contain the number of times a certain date or ID has been encountered
 
-    float total;
-    float total2;
+    float total;        //All sorts : will contain the total of the currently sorted elements for a given ID or date
+    float total2;       
 
-    float average;
-    float average2;
+    float average;      //All sorts : will contain the average value of the currently sorted elements for a given ID or date
+    float average2;     //Only used in wind : wind vector's average y coordinate
 
-    float cos;
-    float sin;
+    float cos;      //Only used in wind : wind vector's x coordinate
+    float sin;      //Only used in wind : wind vector's y coordinate
 
-    Date* date;
+    Date* date;     //Date
 
-    struct _station1* rs;
-    struct _station1* ls;
+    struct _station1* rs;   //Right son
+    struct _station1* ls;   //Left son
 
-    int balance;
+    int balance;    //Only used in AVL : element balance
 
-    float x;
-    float y;
+    float x;        //Longitude coordinates
+    float y;        //Latitude coordinates
 
 } Station1;
 
 typedef struct _node {
-    int ID;
+    int ID;     //Station ID
 
-    float min;
-    float max;
+    float min;      //Will contain min value reached for a certain date or ID
+    float max;      //Will contain max value reached for a certain date or ID
 
-    int count;
+    int count;      //All sorts : will contain the number of times a certain date or ID has been encountered
 
-    float total;
-    float total2;
+    float total;        //All sorts : will contain the total of the currently sorted elements for a given ID or date
+    float total2;       //Only used in wind : total of the wind vector's y coordinate
 
-    float average;
-    float average2;
+    float average;          //All sorts : will contain the average value of the currently sorted elements for a given ID or date
+    float average2;         //Only used in wind : wind vector's average y coordinate
     
-    float cos;
-    float sin;
+    float cos;      //Only used in wind : wind vector's x coordinate
+    float sin;      //Only used in wind : wind vector's y coordinate
 
-    Date* date;   
-    struct _node* pNext;
-    struct _node* pBefore;
+    Date* date;     //Date
 
-    float x;
-    float y;
+    struct _node* pNext;        //Next element
+    struct _node* pBefore;      //Previous element
+
+    float x;        //Longitude coordinates
+    float y;        //Latitude coordinates
 }Node;
 
 typedef struct _list {
-    Node* pFirst;
-    Node* pLast;
+    Node* pFirst;       //List's first element
+    Node* pLast;        //List's last element
 }List;
 
 
@@ -85,14 +86,14 @@ typedef struct _list {
 
 //=======================================================================
 
-int TestFileExist ( char* FN );
+int TestFileExist ( char* FN );         //Check if given files exist
 
-Date* addDate( int year, int month, int day, int hour, int utc);
-int dateComp ( Date* NewDate, Date* currDate );
+Date* addDate( int year, int month, int day, int hour, int utc);        //Create a date structure with given values
+int dateComp ( Date* NewDate, Date* currDate );             //Compare new date and current date
 
-int SortABR( char* preFN , char* postFN , int R, int dSort, int mode);
-int SortLIST(char* preFN , char* postFN , int R, int dSort, int mode);
-int SortAVL( char* preFN , char* postFN , int R, int* pH, int dSort, int mode );
+int SortABR( char* preFN , char* postFN , int R, int dSort, int mode);          //Sort using ABR method
+int SortLIST(char* preFN , char* postFN , int R, int dSort, int mode);          //Sort using Doubly linked list method
+int SortAVL( char* preFN , char* postFN , int R, int* pH, int dSort, int mode );    //Sort using AVL method
 
 //=======================================================================
 
